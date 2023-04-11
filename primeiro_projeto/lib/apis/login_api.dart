a25ad06864b068aabea482677d886de4926d65b4
marcelodasilva1336 <marcelodasilva1336@gmail.com>
2023-04-11 16:22:16 -0300

    aula 17

--- primeiro_projeto/lib/apis/login_api.dart
+++ primeiro_projeto/lib/apis/login_api.dart
@@ -9,7 +9,11 @@ class LoginApi {
     Router router = Router();
 
     router.post('/login', (Request req) async {
-      return Response.ok(await _securityService.generateJWT('1'));
+      // return Response.ok(await _securityService.generateJWT('1'));
+      var token = await _securityService.generateJWT('1');
+      var result = await _securityService.validateJWT(token);
+
+      return Response.ok((result != null).toString());
     });
 
     return router;
